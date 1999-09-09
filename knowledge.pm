8376ea4bb5ea7f9d2bea69a3de7a425208c457e1
Benjamin Holzman <bholzman@gmail.com>
1999-09-09 04:36:16 +0000

    Import of BHOLZMAN/Source-of-all-knowledge-1.0 from CPAN.
    gitpan-cpan-distribution: Source-of-all-knowledge
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         BHOLZMAN/Source-of-all-knowledge-1.0.tar.gz
    gitpan-cpan-author:       BHOLZMAN
    gitpan-cpan-maturity:     released

---
+++ knowledge.pm
@@ -0,0 +1,20 @@
+=head1 NAME
+
+Source::of::all::knowledge - Source of all knowledge
+
+=head1 SYNOPSIS
+
+  use Source::of::all::knowledge;
+
+=head1 DESCRIPTION
+
+All inputs will be respected.
+All output will be reflected.
+No Fortune; No Fever, No Pain
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
+<>
